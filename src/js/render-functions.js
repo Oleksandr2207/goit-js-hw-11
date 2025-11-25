701380d053096db03423ccc79a45cb5f8ce05bae
Oleksandr2207 <osponomarenko09@gmail.com>
2025-11-25 12:55:46 +0200

    hw11

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -14,10 +14,30 @@ export function createGallery(images) {
   const markup = images
     .map(
       img => `
-      <a class="gallery-item" href="${img.largeImageURL}">
-        <img src="${img.webformatURL}" alt="${img.tags}" />
-      </a>
-    `
+      <div class="photo-card">
+        <a class="gallery-item" href="${img.largeImageURL}">
+          <img src="${img.webformatURL}" alt="${img.tags}" />
+        </a>
+        <div class="info">
+          <div class="info-item">
+            <b>Likes</b>
+            <span>${img.likes}</span>
+          </div>
+          <div class="info-item">
+            <b>Views</b>
+            <span>${img.views}</span>
+          </div>
+          <div class="info-item">
+            <b>Comments</b>
+            <span>${img.comments}</span>
+          </div>
+          <div class="info-item">
+            <b>Downloads</b>
+            <span>${img.downloads}</span>
+          </div>
+        </div>
+      </div>
+      `
     )
     .join('');
 
@@ -25,6 +45,7 @@ export function createGallery(images) {
 
   lightbox.refresh();
 }
+
 
 export function clearGallery() {
   galleryContainer.innerHTML = '';
@@ -34,9 +55,9 @@ export function clearGallery() {
 
 
 export function showLoader() {
-  loader.classList.add('visible');
+  loader.classList.add('show');
 }
 
 export function hideLoader() {
-  loader.classList.remove('visible');
+  loader.classList.remove('show');
 }
